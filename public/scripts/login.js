13d66fdee28a2d2fc53c1e20745961de33b913f6
shchik <nikshc2005@mail.ru>
2024-10-28 16:10:56 +0300

    created error message under login

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -12,6 +12,12 @@ function enterAccount() {
     success: (response) => {
       if (response.status === "success") {
         window.location.href = "index.html";
+      } else {
+        document
+          .querySelector(".login-message")
+          .classList.add("error-login-message");
+        document.querySelector(".login-message").innerHTML =
+          "Неверный логин или пароль!";
       }
     },
     error: (xhr, status, error) => {
