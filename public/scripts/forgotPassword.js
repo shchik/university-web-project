ac4ed8ea040c7020841a9706e4ff716f0aab91bf
shchik <nikshc2005@mail.ru>
2024-10-29 16:54:07 +0300

    added phpmailer to project

---
+++ public/scripts/forgotPassword.js
@@ -0,0 +1,31 @@
+const EMAIL_REGEXP =
+  /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+document.querySelector(".button-message").addEventListener("click", () => {
+  const email = document.getElementById("email").value;
+  if (isEmailValid(email)) {
+    document.querySelector(
+      ".resend-message"
+    ).innerHTML = `<a class="resend-again" href="">Отправить код повторно</a>`;
+    console.log(email);
+    $.ajax({
+      url: "../../../public/phpScripts/resetPassword.php",
+      data: {
+        email: email,
+      },
+      type: "POST",
+      success: (response) => {
+        console.log("Ответ сервера:", response);
+      },
+      error: (xhr, status, error) => {
+        console.error("Ошибка:", error);
+      },
+    });
+  } else {
+    alert("email isnt valid!");
+  }
+});
+
+function isEmailValid(value) {
+  return EMAIL_REGEXP.test(value);
+}
